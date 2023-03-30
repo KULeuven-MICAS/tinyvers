c2a607b926fa376b6437a8642a5c85cfca939a7b
Vikram Jain <vikramjainiit@gmail.com>
2023-03-30 19:27:01 +0200

    some more changes

--- ips/hwpe-mac-engine/rtl/SRAM_parametrizable_w_equivalent.v
+++ ips/hwpe-mac-engine/rtl/SRAM_parametrizable_w_equivalent.v
@@ -19,7 +19,7 @@ module SRAM_parametrizable_w_equivalent (
 	output reg [numBit - 1:0] Q;
 	wire CLK_gated;
 	MEMS1D_BUFG_512x32_wrapper SRAM_i(
-		.CLK(CLK_gated),
+		.CLK(CLK),
 		.D(D),
 		.AS(A[8]),
 		.AW(A[7:2]),
@@ -28,11 +28,5 @@ module SRAM_parametrizable_w_equivalent (
 		.RDWEN(WEB),
 		.BW(1'sb1),
 		.Q(Q)
-	);
-	pulp_clock_gating i_clk_gate_l1_wt(
-		.clk_i(CLK),
-		.en_i(~scan_en_in),
-		.test_en_i(1'b0),
-		.clk_o(CLK_gated)
 	);
 endmodule
